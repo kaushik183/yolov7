12348d6d647d8ce6ac93b07eae90bbb9e8fd53f8
kaushik183 <87715402+kaushik183@users.noreply.github.com>
2022-08-10 13:38:19 +0530

    Update google_utils.py

--- utils/google_utils.py
+++ utils/google_utils.py
@@ -23,8 +23,8 @@ def attempt_download(file, repo='WongKinYiu/yolov7'):
     if not file.exists():
         try:
             response = requests.get(f'https://api.github.com/repos/{repo}/releases/latest').json()  # github api
-            assets = [x['name'] for x in response['assets']]  # release assets
-            tag = response['tag_name']  # i.e. 'v1.0'
+            #assets = [x['name'] for x in response['assets']]  # release assets
+            #tag = response['tag_name']  # i.e. 'v1.0'
         except:  # fallback plan
             assets = ['yolov7.pt', 'yolov7-tiny.pt', 'yolov7x.pt', 'yolov7-d6.pt', 'yolov7-e6.pt', 
                       'yolov7-e6e.pt', 'yolov7-w6.pt']
